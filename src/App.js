c56e1522c1ecbb4859a76532028b80e1bd61df06
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-06-06 20:55:15 -0400

    Added react-poll and created a test poll.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Poll from 'react-polls';
+
+const pollQuestion = 'Does this work?';
+const pollAnswers = [
+  { option: 'Yes', votes: 0},
+  { option: 'No', votes: 0}
+]
 
 class App extends Component {
+
+  state = {
+    pollAnswers: [...pollAnswers]
+  }
+
+  handleVote = (voteAnswer) => {
+    const { pollAnswers } = this.state
+    const newPollAnswers = pollAnswers.map(answer => {
+      if (answer.option === voteAnswer) {
+        answer.votes++
+        return answer
+      }
+    })
+    this.setState({ pollAnswers: newPollAnswers })
+  }
+
 
   render() {
     return (
       <div className="App">
-        <h1>hi!</h1>
+        <Poll question={pollQuestion}
+              answers={pollAnswers}
+              onVote={this.handleVote}
+              customStyles={{ theme: 'purple' }}/>
       </div>
     );
   }
-  
+
 }
 
 export default App;
